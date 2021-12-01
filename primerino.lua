332a230f39c9619042663da9840efc8e21f9ec53
Binomica-Labs <sebastian@binomicalabs.org>
2021-12-01 20:44:48 +0000

    working detection primer generation (super basic)

--- primerino.lua
+++ primerino.lua
@@ -37,51 +37,33 @@ end
 
 
 local function parseFasta(fileName)
-	file = io.open(fileName, "r")
+	file = io.open(fileName, "r")	--declare a file variable and open said passed file by its name
 	local header = file:read()	--read first line of file, store as FASTA header variable
-	local fileContent = file:read("*all")
-	file:close()
-	local trimmedSeq = string.gsub(fileContent, header, "")
-	local polishedSeq = string.upper(trimmedSeq:gsub("[\r\n]", "")) 
-	return header, polishedSeq
+	local fileContent = file:read("*all")	--store the entire contents of the file as a single string
+	file:close()							--close the file
+	local trimmedSeq = string.gsub(fileContent, header, "")	--remove the FASTA header
+	local polishedSeq = string.upper(trimmedSeq:gsub("[\r\n]", ""))	--remove all carriage returns so the actual sequence is a single string
+	return header, polishedSeq	--return these two variables to be used later
 end
 
 
 
 local function makeDetectionPrimers()
-	local refName = ""
-	local inputRefSeq = ""
 	local inputAmplicon = ""
-	local refSeq = ""
 	local amplicon = ""
-	local detectionFile = io.open("DET", "w")
+	local detectionFile = io.open("DET", "w")	--make a temporary file called DET and make it writeable
 	
-	os.execute("clear")		
+	os.execute("clear")		--clear the terminal window
 
 	print("Welcome to an interactive Lua wrapper for Primer3!")
 	print("")
---[[	print("Let's start by telling me the sequence or file name you wish to use as a reference.")
-	print("This can be any DNA sequence in FASTA format. If you want to copy/paste a sequence manually, you can do that as well.")
-	print("")
-	
-	io.write("Please type in a file name or sequence here: ")
-	inputRefSeq = io.read()
-	fileRefSeq = io.open(inputRefSeq, "r")
-	if fileRefSeq then
-		fileRefSeq:close()
-		refName, refSeq = parseFasta(inputRefSeq)
-	else
-		refSeq = inputRefSeq	
-	end	
-	
-	print("")
-]]--
+
 	print("Tell me the file name or, if you feel brave, copy and paste the sequence")
 	print("information directly. Be sure to use only single FASTA entries for now.")
 
 	io.write("Please type in a file name or sequence here: ")
 
-	inputAmplicon = io.read()
+	inputAmplicon = io.read()	--store user input as amplicon reference
 	fileAmplicon = io.open(inputAmplicon, "r")
 	if fileAmplicon then
 		fileAmplicon:close()
@@ -91,7 +73,7 @@ local function makeDetectionPrimers()
 	end
 	
 	print("")
-	--print("length of refSeq: " .. refSeq:len())
+
 	print("Input Sequence Length: " .. amplicon:len())
 	print("")
 	print("At what temp do you wish to anneal your primers? Default is 60 degrees Celsius.")
@@ -103,11 +85,12 @@ local function makeDetectionPrimers()
 	print("How long should your oligos be? Typically 25bp gives nice unique sequences.")
 
 	io.write("Please type in your desired oligo length here: ")
-	local idealLength = io.read()
+	local idealPrimerLength = io.read()
 
-	print("Cool. I'll write up the necessary file to feed into Primer3. Please make sure you have Primer3 installed.")
+	print("Cool. I'll write up the necessary file to feed into Primer3. One moment, please!")
 	
-	local settingsList = {}
+	local settingsList = {}		--table for storing the Primer3 input file settings and user inputs
+
 
 	settingsList[1] = "SEQUENCE_ID=" .. "detectionTest" .. "\n"
 	settingsList[2] = "SEQUENCE_TEMPLATE=" .. amplicon .. "\n"
@@ -115,11 +98,11 @@ local function makeDetectionPrimers()
 	settingsList[4] = "PRIMER_PICK_LEFT_PRIMER=1" .. "\n"
 	settingsList[5] = "PRIMER_PICK_INTERNAL_OLIGO=0" .. "\n"
 	settingsList[6] = "PRIMER_PICK_RIGHT_PRIMER=1" .. "\n"
-	settingsList[7] = "PRIMER_OPT_SIZE=" .. idealLength .. "\n"
+	settingsList[7] = "PRIMER_OPT_SIZE=" .. idealPrimerLength .. "\n"
 	settingsList[8] = "PRIMER_MIN_SIZE=18" .. "\n"
 	settingsList[9] = "PRIMER_MAX_SIZE=36" .. "\n"	--Primer3 hard capped at 36bp
-	settingsList[10] = "PRIMER_PRODUCT_SIZE_RANGE=450-550" .. "\n"
-	settingsList[11] = "PRIMER_EXPLAIN_FLAG=0" .. "\n"
+	settingsList[10] = "PRIMER_PRODUCT_SIZE_RANGE=450-550" .. "\n"	--amplicon size rage, will port this out soon
+	settingsList[11] = "PRIMER_EXPLAIN_FLAG=0" .. "\n"	--shut off verbose mode for easy parsing later
 	settingsList[12] = "=" .. "\n" --necessary end character
 
 	for i=1, #settingsList do
@@ -127,11 +110,15 @@ local function makeDetectionPrimers()
 	end
 	detectionFile:close()
 	
-	os.execute("./primer3 DET\n")	--run primer3's core binary from the terminal using file called "DET" (temporary settings file)
+	os.execute("./primer3 DET > detectionPrimers.txt\n")	--run primer3's core binary from the terminal using file called "DET" (temporary settings file)
+	os.execute("rm DET")	--remove temporary Primer3 input file; comment out if you want to keep it
+	print("")
+	print("")
+	print("Done! See file detectionPrimers.txt for the raw Primer3 output.")
+	print("Output parser and CSV generator comming soon.")
+	print("Enjoy your day! :)")
 end 
 
 if arg[1] == "detect" then
 	makeDetectionPrimers()
 end
-
-
